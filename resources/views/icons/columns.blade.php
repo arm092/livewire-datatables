bb5b2176e3d1aad77521577740ae05cbfbccc022
Arman <arman.khachatryan.dev@gmail.com>
2026-09-26 00:53:39 +0400

    Polish filter and toolbar interactions

---
+++ resources/views/icons/columns.blade.php
@@ -0,0 +1,3 @@
+<svg {{ $attributes->merge(['class' => 'h-5 w-5 stroke-current']) }} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 5h16v14H4zM9.333 5v14m5.334-14v14" />
+</svg>
